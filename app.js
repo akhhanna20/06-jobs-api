d431b8ed8080edac06a924c46b359363c9710122
Hanna <akhhanna20@gmail.com>
2025-01-13 08:52:41 -0500

    Answers fo lesson Jobs API-Part 1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 //connect DB
 const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
 connectDB(process.env.MONGO_URI);
 // routers
 const authRouter = require("./routes/auth");
@@ -13,13 +14,14 @@ const jobsRouter = require("./routes/jobs");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const auth = require("./middleware/authentication");
 
 app.use(express.json());
 // extra packages
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
